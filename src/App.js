b3a1f479fd6ab6f0635c9ee1ae15ace0a0e9ff52
21CEUOS101 <21ceuos101@ddu.ac.in>
2024-07-01 13:54:22 +0530

    Project Started With Initial Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+import "./styles/globals.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
